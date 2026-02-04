465317e8b50ade726eae34a0989dadbe22d7f33b
CadenFinley <caden.finley0789@gmail.com>
2026-02-04 02:10:33 -0600

    vertical

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -14,6 +14,7 @@ map({ "n", "i" }, "<C-s>", "<Cmd>w<CR>", { desc = "Save file" })
 map("n", "<C-q>", "<Cmd>q<CR>", { desc = "Quit window" })
 map("n", "<leader>l", "<Cmd>Lazy<CR>", { desc = "Open Lazy" })
 map("n", "<leader>e", "<Cmd>NvimTreeToggle<CR>", { desc = "Toggle file explorer" })
+map("n", "<leader>vs", "<Cmd>vsplit<CR>", { desc = "Vertical split" })
 
 map("n", "<leader>f", telescope_pick("find_files"), { desc = "Find files" })
 map("n", "<leader>g", telescope_pick("live_grep"), { desc = "Search project" })
