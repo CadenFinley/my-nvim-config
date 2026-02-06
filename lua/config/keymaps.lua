46223e8e799ef30bc0a07d3f2604bdaeac38a587
CadenFinley <caden.finley0789@gmail.com>
2026-02-06 00:37:02 -0600

    bump bump bump o

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -10,8 +10,27 @@ local function telescope_pick(name)
   end
 end
 
+local function open_last_buffer_or_file()
+  local alt_buf = vim.fn.bufnr("#")
+  if alt_buf > 0 and vim.fn.buflisted(alt_buf) == 1 then
+    vim.cmd("buffer #")
+    return
+  end
+
+  local oldfiles = vim.v.oldfiles or {}
+  for _, file in ipairs(oldfiles) do
+    if file ~= "" and vim.fn.filereadable(file) == 1 then
+      vim.cmd("edit " .. vim.fn.fnameescape(file))
+      return
+    end
+  end
+
+  vim.notify("No previous buffer or recent file found", vim.log.levels.WARN)
+end
+
 map({ "n", "i" }, "<C-s>", "<Cmd>w<CR>", { desc = "Save file" })
 map("n", "<C-q>", "<Cmd>q<CR>", { desc = "Quit window" })
+map("n", "<leader><leader>", open_last_buffer_or_file, { desc = "Last buffer or recent file" })
 map("n", "<leader>l", "<Cmd>Lazy<CR>", { desc = "Open Lazy" })
 map("n", "<leader>e", "<Cmd>NvimTreeToggle<CR>", { desc = "Toggle file explorer" })
 map("n", "<leader>vs", "<Cmd>vsplit<CR>", { desc = "Vertical split" })
